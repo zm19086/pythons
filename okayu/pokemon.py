745a6771d213d793891d8f3844818da9f29b6930
zm19086 <zm19086@josai.ac.jp>
2022-11-06 00:47:32 +0900

    update pythons/okayu/pokemon.py

--- okayu/pokemon.py
+++ okayu/pokemon.py
@@ -19,18 +19,6 @@ class pokemon:
   def lottery(self):
     return self.aim
 
-class battle(pokemon):
-  def __init__(self, myHp, myAtc, enemyHp, enemyAtc):
-    self.myHp = myHp
-    self.myAtc = myAtc
-    self.enemyHp = enemyHp
-    self.enemyAtc = enemyAtc
-  
-  def btl(self):
-    atc = self.enemyAtc - self.myAtc
-    defense = self.myHp - self.enemyAtc
-    return atc, defense
-
 kodakku = pokemon('コダック', 60, 12, 4)
 sonansu = pokemon('ソーナンス', 65, 10, 2)
 pikachu = pokemon('ピカチュウ', 50, 15, 5)
@@ -38,12 +26,24 @@ mukkuru = pokemon('ムックル', 45, 18, 3)
 yadon = pokemon('ヤドン', 58, 11, 1)
 
 myPokemon = kodakku
+myPokemonHp = kodakku.hitPoint()
+myPokemonAtc = kodakku.attack()
 enemyPokemon = mukkuru
+enemyPokemonHp = mukkuru.hitPoint()
+enemyPokemonAtc = mukkuru.attack()
+
+def battle():
+  while (myPokemonHp <= 0 or enemyPokemonHp <= 0):
+    # 乱数抽選
+    # 乱数をもとに条件分岐
+    print('a')
+    myPokemonHp = myPokemonHp - enemyPokemonAtc
+    
+    # 乱数抽選
+    # 乱数をもとに条件分岐
+    print('b')
+    enemyPokemonHp = enemyPokemonHp - myPokemonAtc
+
+battle()
 
 
-
-msg1 = myPokemon.getName() + 'のこうげき！'
-msg2 = enemyPokemon.getName() + 'に、' + str(myPokemon.attack()) + 'ダメージ！'
-
-print(msg1)
-print(msg2)
