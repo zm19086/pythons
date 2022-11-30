f884dbc96c3a7d56deb911450eb8698c7077fda4
zm19086 <zm19086@josai.ac.jp>
2022-11-30 13:27:38 +0900

    Update task1~5

--- don/task/task1_print.py
+++ don/task/task1_print.py
@@ -1,13 +1,16 @@
-# print()で値を表示しよう sample_folder/function.py line73~
+# 1 print()で値を表示しよう sample_folder/function.py line73~
 
+# 1-1
 # 「12345」と表示
 
 
 
+# 1-2
 # 「あいうえお」と表示
 
 
 
-# 2 + 3 を使って「5」と表示
+# 1-3
+# 足し算を使って「5」と表示
 
 
---
+++ don/task/task5_if.py
@@ -0,0 +1,16 @@
+# 5 条件分岐で結果を変えよう sample_folder/if.py line11~
+
+# 5-1
+# 変数 flg = False を定義し、flgがTrueなら「good」、Falseなら「bad」を表示
+
+
+
+
+
+
+
+# 5-2
+# 変数 age = 20
+# 変数 studentLicense = True
+# 変数 time = 19
+# これらの変数を定義し、16歳以上かつ、学生証を持っていて、18時以降の利用なら割引されるサービスを作る
